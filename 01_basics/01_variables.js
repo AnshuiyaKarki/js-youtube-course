c117a3808093e45d801336cc68c98f19fd3262e6
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-07-02 11:14:06 +0000

    learning variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID=1434
+let accountEmail="anshuiyakarki@google.com"
+var accountPassword="1234"
+accountCity="Pune"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
