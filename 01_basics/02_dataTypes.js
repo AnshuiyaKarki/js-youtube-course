5f7ec1d6d08bac47ddbda8802c9f35cea7c26e61
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-07-13 08:42:44 +0000

    Datatypes and ECMA

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"// treats JS as newer version
+//alert(3+3)// node
+let name="Anshuiya"
+let age=21
+let LoggedIn=true;
+
+//string 
+//number->2^53
+//bigint
+//boolean
+//undefined
+//null
+//symbol->unique
+//object
+
+console.log(LoggedIn)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
