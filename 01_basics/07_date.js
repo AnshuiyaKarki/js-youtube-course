1bbba43d636919271813046eb1b7b41db7b6cf05
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-08-12 09:39:26 +0000

    date and time

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -14,3 +14,21 @@ console.log(myNewCreatedDate.toString())
 
 let myAnotherDate=new Date('2026-01-12')//manual entry then months are 1 based 
 console.log(myAnotherDate.toLocaleString())
+
+
+console.log(myCreatedDate.getDay())
+
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp) //returns the number of milliseconds that have passed since January 1, 1970, 00:00:00 UTC
+//to get seconds divide by 1000 cause 1s=1000ms
+
+console.log(myTimeStamp/1000 + " seconds")
+console.log(Math.floor(myTimeStamp/1000 )+ " seconds")
+
+let newDate= new Date('2023-03-15')
+console.log(newDate)
+console.log(newDate.getDay()+1)//day is 0 based indexing while getting it so  we do +1 to get exact day index of the week
+console.log(newDate.toLocaleString('default',{
+    weekday:'long'
+}))
