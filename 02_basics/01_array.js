7fcdf3a2eb0ba9dd23f2f7b2c95801b5754ee7b9
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-08-12 14:01:33 +0000

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+//array(Non primitive data type)
+
+const myArr=[0,1,2,3,4,5]
+const myHeros=['Scarlet Witch', 'Thor']
+console.log(myArr)
+
+//another way of creating an array 
+const newArr= new Array(1,2,3,4,5)
+console.log(newArr)
+
+//accessing array elements 
+console.log(myArr[1])
+
+
+//Array Methods
+newArr.push(7)
+console.log(newArr) //original array value change 
+
+newArr.pop()
+console.log(newArr) 
+
+
