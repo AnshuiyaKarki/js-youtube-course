21634b225e3f0a4c8de03e76d09dd4917500c2ec
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-08-17 09:36:28 +0000

    array basics

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -18,3 +18,15 @@
  const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
  const real_another_array=another_array.flat(Infinity)
  console.log(real_another_array)
+
+ console.log(Array.isArray("Anshuiya"))
+console.log(Array.from("Anshuiya"))
+console.log(Array.from({name: "hitesh"})) // interesting returns empty it is just a normal object what needs to be in the array needs to be specified
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+// of() puts your arguments INTO an array.
+// from() takes elements OUT OF something and puts them into an array.
