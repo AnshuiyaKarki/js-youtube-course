6a195fee3837ad8162176fe6c00f23355a8d64ce
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-08-17 09:01:05 +0000

    array basics

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -17,6 +17,20 @@ newArr.push(7)
 console.log(newArr) //original array value change 
 
 newArr.pop()
-console.log(newArr) 
+console.log(newArr)
+
+myArr.unshift(10)// adds elements to the front
+console.log(myArr) 
+
+myArr.shift()//removes elements from the front
+console.log(myArr)
+
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(7))
+
+const newAr=myArr.join()//adds all elements of an array to string seperated by comma
+console.log(newAr)
+console.log(typeof newAr)
+
 
 
