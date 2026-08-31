74a2a78e9cadf1b1995fd4616515da1707a36fd7
AnshuiyaKarki <anshuiyakarki01@gmail.com>
2026-08-31 08:38:10 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+let i=0
+while(i<=10){
+    console.log("value of infex is ",i)
+    i++
+}
+let index=0
+let myArray=['flash','batman','superman']
+while(index<myArray.length){
+    console.log(myArray[index])
+    index++
+}
+
+let score=11
+
+do{
+ console.log("demonstration of do while loop, score: ",score)
+}while (score<=10)
